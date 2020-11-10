1a34e3cc3ed2889a6471b3274252f1fee9426b90
Srikesh Srinivas <ssrinivas45@gatech.edu>
2020-11-10 18:59:05 -0500

    map fixes; 2006 fire analysis

--- utilities.py
+++ utilities.py
@@ -7,10 +7,10 @@ def load_data(fire_csv="datasets/fires.csv", aqi_csv="datasets/aqi.csv"):
             {"STATE_NAME": str, "COUNTY_NAME": str,"STATE_CODE": int,"COUNTY_CODE": int,"DATE": object, \
              "AQI": int,"CATEGORY": str,"DEFINING_PARAMETER": str, "DEFINING_SITE": str,"NUMBER_OF_SITES_REPORTING":int,
              "YEAR":int})
-    
+
     columns = ["FIRE_NAME", "FIRE_YEAR", "FIRE_SIZE", "DISCOVERY_DATE", "CONT_DATE", "STAT_CAUSE_DESCR", \
                "STATE_NAME", "COUNTY_NAME", "STATE_CODE", "COUNTY_CODE"]
-    
+
     fires = fires[columns]
     aqi['DATE'] = pd.to_datetime(aqi['DATE'])
     return fires, aqi
@@ -50,13 +50,13 @@ def get_aqi(df, state=None, state_code=None, county=None, county_code=None, date
 
 
 def compare_aqi_with_year(df, state=None, state_code=None, county=None, county_code=None, date=None, days_before=0, days_after=None, year=None):
-    
+
     aqi_df = get_aqi(df, state=state, state_code=state_code, county=county, county_code=county_code, date=date, days_before=days_before, \
                      days_after=days_after, year=year)
-    
+
     if len(aqi_df) == 0:
         return None, None
-    
+
     year = aqi_df.iloc[0]['YEAR']
     avg_year_aqi = get_aqi(df, year=year).AQI.mean()
     avg_aqi = aqi_df.AQI.mean()
@@ -65,32 +65,57 @@ def compare_aqi_with_year(df, state=None, state_code=None, county=None, county_c
 
 def compare_aqi_for_fire(fires, aqi, fire_id, days=(0,14)):
     entry = fires.iloc[fire_id]
-    
+
     name, state_code, county_code, date = entry.FIRE_NAME, entry.STATE_CODE, entry.COUNTY_CODE, entry.DISCOVERY_DATE
     state_name, county_name = entry.STATE_NAME, entry.COUNTY_NAME
     aqi_avg, aqi_year_avg = compare_aqi_with_year(aqi, state_code=state_code, county_code=county_code, date=date, days_before=days[0], days_after=days[1])
-    
+
     return aqi_avg, aqi_year_avg
-    
+
 def get_aqi_for_fire(fires, aqi, fire_id, days=(0,14)):
     entry = fires.iloc[fire_id]
-    
+
     name, state_code, county_code, date = entry.FIRE_NAME, entry.STATE_CODE, entry.COUNTY_CODE, entry.DISCOVERY_DATE
     state_name, county_name = entry.STATE_NAME, entry.COUNTY_NAME
     aqi_df = get_aqi(aqi, state_code=state_code, county_code=county_code, date=date, days_before=days[0], days_after=days[1])
-    
+
     return aqi_df
-    
-def get_aqi_change(aqi, row, days):
+
+def get_aqi_change(aqi, row, days, year=None):
     date = row.DISCOVERY_DATE
     state_code = row.STATE_CODE
     county_code = row.COUNTY_CODE
     days_before = days[0]
     days_after = days[1]
-    
-    avg_aqi, avg_year_aqi = compare_aqi_with_year(aqi, state_code=state_code, county_code=county_code, date=date, days_before=days_before, days_after=days_after)
+
+    avg_aqi, avg_year_aqi = compare_aqi_with_year(aqi, state_code=state_code, county_code=county_code, date=date, days_before=days_before, days_after=days_after, year=year)
     if avg_aqi is None or avg_year_aqi is None:
         return None
-    
+
     return (avg_aqi-avg_year_aqi)/avg_year_aqi
-    
+
+def get_aqi_change2(aqi, row, year=None):
+    date = row.DISCOVERY_DATE
+    state_code = row.STATE_CODE
+    county_code = row.COUNTY_CODE
+    days_before = 0
+    days_after = row.DAYS
+
+    avg_aqi, avg_year_aqi = compare_aqi_with_year(aqi, state_code=state_code, county_code=county_code, date=date, days_before=days_before, days_after=days_after, year=year)
+    if avg_aqi is None or avg_year_aqi is None:
+        return None
+
+    return (avg_aqi-avg_year_aqi)/avg_year_aqi
+
+def get_aqi_avg(aqi, row, days, year=None):
+    date = row.DISCOVERY_DATE
+    state_code = row.STATE_CODE
+    county_code = row.COUNTY_CODE
+    days_before = days[0]
+    days_after = days[1]
+
+    avg_aqi, avg_year_aqi = compare_aqi_with_year(aqi, state_code=state_code, county_code=county_code, date=date, days_before=days_before, days_after=days_after, year=year)
+    if avg_aqi is None or avg_year_aqi is None:
+        return None
+
+    return avg_aqi
