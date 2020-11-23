10903db44f3617126e632dd43e910dcd9f7f72c5
Wendell Hom <wendell_hom@yahoo.com>
2020-11-22 17:18:15 -0800

    Add year column

--- Software/CODE/scripts/aqi_col_rename.py
+++ Software/CODE/scripts/aqi_col_rename.py
@@ -5,4 +5,5 @@ import csv
 for i in range(1993,2016):
     df = pd.read_csv("daily_aqi_by_county_" + str(i) + ".csv", dtype={'State Code': str, 'County Code': str, 'AQI':int, 'NUMBER_OF_SITES_REPORTING': int})
     df.columns = ["STATE_NAME","COUNTY_NAME","STATE_CODE","COUNTY_CODE","DATE","AQI","CATEGORY","DEFINING_PARAMETER","DEFINING_SITE","NUMBER_OF_SITES_REPORTING"]
+    df['YEAR'] = pd.DatetimeIndex(df['DATE']).year
     df.to_csv("daily_aqi_by_county_" + str(i) + ".csv", index=False, quoting=csv.QUOTE_NONNUMERIC)
